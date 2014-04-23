c1b03b6b841fd89a9a5527c9264f3a0d3d7e2832
artothief <wolden@gmail.com>
2014-04-23 21:07:41 +0200

    Annulus volume integrated, bug if you only use hwdp to bit depth. Calculations need to be checked.

--- volumes.py
+++ volumes.py
@@ -16,17 +16,14 @@ class Volumes:
         self.window = self.builder.get_object('window1')
         self.window.show_all()
 
-        #Seabed, bit depth, casing shoe and riser capacity
+        #Seabed, bit depth, casing shoe + casing and riser capacity
         self.seabed_entry = self.builder.get_object('seabed_entry')
-        self.seabed = float(self.seabed_entry.get_text())
-        print 'Seabed = ' + str(self.seabed)
         self.riser_cap_label = self.builder.get_object('riser_cap_label')
         self.riser_cap = float(self.riser_cap_label.get_text())
         print 'Riser cap = ' + str(self.riser_cap)
         self.bit_depth_entry = self.builder.get_object('bit_depth_entry')
-        self.bit_depth = float(self.bit_depth_entry.get_text())
         self.csg_shoe_entry = self.builder.get_object('csg_shoe_entry')
-        self.csg_shoe = float(self.csg_shoe_entry.get_text())
+        self.csg_cap_entry = self.builder.get_object('csg_cap_entry')
 
         #tubular info
         self.p_box = self.builder.get_object('p_box')
@@ -44,6 +41,8 @@ class Volumes:
         self.linerstore = self.builder.get_object('liststore1')
         self.riser_vol_label = self.builder.get_object('riser_btms_up_label')
         self.riser_stroke_label = self.builder.get_object('riser_strokes_label')
+        self.shoe_strokes_label = self.builder.get_object('shoe_strokes_label')
+        self.shoe_btms_up_label = self.builder.get_object('shoe_btms_up_label')
 
     #Calculated pipe length
     def pipe_length(self):
@@ -80,9 +79,15 @@ class Volumes:
         self.dc_act = self.dc_box.get_active()
         self.dc_ce_cap = self.dc_store[self.dc_act] [2]
         self.hwdp_act = self.hwdp_box.get_active()
-        self.hwdp_ce_cap = self.hwdp_store[self.hwdp_act] [1]
+        self.hwdp_ce_cap = self.hwdp_store[self.hwdp_act] [2]
         self.p_cap = self.p_store[self.p_act] [1]
-        self.dp_ce_cap = self.p_store[self.p_act] [1]
+        self.dp_ce_cap = self.p_store[self.p_act] [2]
+        self.seabed = float(self.seabed_entry.get_text())
+        print 'Seabed = ' + str(self.seabed)
+        self.bit_depth = float(self.bit_depth_entry.get_text())
+        self.csg_cap = float(self.csg_cap_entry.get_text())
+        self.csg_shoe = float(self.csg_shoe_entry.get_text())
+
 
         if self.dp_length >= self.seabed:
             self.riser_dp_vol = (self.riser_cap - self.dp_ce_cap) * self.seabed
@@ -126,6 +131,59 @@ class Volumes:
         print 'Riser/DC vol = ' + str(self.riser_dc_vol)
         return self.riser_dc_vol
 
+    #Pipe / Csg volume
+    def pipe_csg_vol(self):
+
+
+        if self.dp_length > self.csg_shoe:
+            self.pipe_csg_vol = (self.csg_cap - self.dp_ce_cap) * (self.csg_shoe - self.seabed)
+            
+        elif self.dp_length > self.seabed and self.dp_length < self.csg_shoe:
+            self.pipe_csg_vol = (self.csg_cap - self.dp_ce_cap) * (self.dp_length - self.seabed)
+            
+        else:
+            self.pipe_csg_vol = 0
+
+        print 'Csg/DP vol = ' + str(self.pipe_csg_vol)
+        return self.pipe_csg_vol
+
+    #HWDP / Csg volume
+    def hwdp_csg_volu(self):
+        
+        if self.dp_length > self.seabed and self.dp_length + self.hwdp_length < self.csg_shoe:
+            self.hwdp_csg_vol = (self.csg_cap - self.hwdp_ce_cap) * self.hwdp_length
+            res = 1
+        elif self.dp_length < self.seabed and self.dp_length + self.hwdp_length > self.seabed and self.dp_length + self.hwdp_length < self.csg_shoe:
+            self.hwdp_csg_vol = (self.csg_cap - self.hwdp_ce_cap) * ((self.dp_length + self.hwdp_length) - self.seabed)
+            res = 2
+        elif self.dp_length > self.seabed and self.dp_length < self.csg_shoe and self.dp_length + self.hwdp_length > self.csg_shoe:
+            self.hwdp_csg_vol = (self.csg_cap - self.hwdp_ce_cap) * (self.csg_shoe - self.dp_length)
+            res = 3
+        else:
+            self.hwdp_csg_vol = 0
+            res = 4
+        print res
+        print 'Csg/HWDP vol = ' + str(self.hwdp_csg_vol)
+        return self.hwdp_csg_vol
+            
+    #DC / Csg volume
+    def dc_csg_vol(self):
+         
+        if self.dp_length + self.hwdp_length < self.seabed and self.bit_depth > self.seabed:
+            self.dc_csg_vol = (self.csg_cap - self.dc_ce_cap) * (self.bit_depth - self.seabed)
+        
+        elif self.dp_length + self.hwdp_length > self.seabed and self.dp_length + self.hwdp_length < self.csg_shoe and self.bit_depth < self.csg_shoe:
+            self.dc_csg_vol = (self.csg_cap - self.dc_ce_cap) * self.dc_length
+            
+        elif self.dp_length + self.hwdp_length < self.csg_shoe and self.bit_depth > self.csg_shoe:
+            self.dc_csg_vol = (self.csg_cap - self.dc_ce_cap) * (self.csg_shoe - (self.dp_length + self.hwdp_length))
+            
+        else:
+            self.dc_csg_vol = 0
+
+        print 'Csg/DC vol = ' + str(self.dc_csg_vol)
+        return self.dc_csg_vol
+
     def on_window1_delete_event(self, *args):
         Gtk.main_quit()
 
@@ -141,7 +199,10 @@ class Volumes:
         self.riser_vol_label.set_text(str(round(self.riser_volume)) + ' Litres' )
         self.riser_strokes = self.riser_volume / self.liner_cap
         self.riser_stroke_label.set_text(str(int(self.riser_strokes)) + ' Strokes')
-
+        self.csg_vol = Volumes.pipe_csg_vol(self) + Volumes.hwdp_csg_volu(self) + Volumes.dc_csg_vol(self)
+        self.shoe_btms_up_label.set_text(str(int(self.csg_vol)) + ' Litres')
+        self.csg_strokes = self.csg_vol / self.liner_cap
+        self.shoe_strokes_label.set_text(str(int(self.csg_strokes)) + ' Strokes')
 
 main = Volumes()
 Gtk.main()
