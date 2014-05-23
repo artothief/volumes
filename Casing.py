3345166f5a511c5955c782ca703ac094eaf2f7fc
artothief <wolden@gmail.com>
2014-05-23 21:49:45 +0200

    Modulated Annulus volume, will make easier implementations, need to work on readability

---
+++ Casing.py
@@ -0,0 +1,59 @@
+__author__ = 'artothief'
+
+
+#Pipe / Csg volume
+def dp_csg(seabed, csg_shoe, csg_cap, dp_length, dp_ce_cap):
+
+    if dp_length >= csg_shoe:
+        pipe_csg_vol = (csg_cap - dp_ce_cap) * (csg_shoe - seabed)
+
+    elif dp_length > seabed and dp_length < csg_shoe:
+        pipe_csg_vol = (csg_cap - dp_ce_cap) * (dp_length - seabed)
+
+    else:
+        pipe_csg_vol = 0
+
+    print 'Csg/DP vol = ' + str(pipe_csg_vol)
+    return pipe_csg_vol
+
+#HWDP / Csg volume
+def hwdp_csg(seabed, csg_shoe, csg_cap, dp_length, hwdp_length, hwdp_ce_cap):
+
+    if dp_length > seabed and dp_length + hwdp_length < csg_shoe:
+        hwdp_csg_vol = (csg_cap - hwdp_ce_cap) * hwdp_length
+
+    elif (dp_length < seabed and dp_length + hwdp_length > seabed and
+dp_length + hwdp_length < csg_shoe):
+        hwdp_csg_vol = (csg_cap - hwdp_ce_cap) * ((dp_length + hwdp_length) - seabed)
+
+    elif (dp_length > seabed and dp_length < csg_shoe and
+dp_length + hwdp_length > csg_shoe):
+        hwdp_csg_vol = (csg_cap - hwdp_ce_cap) * (csg_shoe - dp_length)
+
+    elif hwdp_length >= seabed and hwdp_length >= csg_shoe:
+        hwdp_csg_vol = (csg_cap - hwdp_ce_cap) * hwdp_length
+
+    else:
+        hwdp_csg_vol = 0
+
+    print 'Csg/HWDP vol = ' + str(hwdp_csg_vol)
+    return hwdp_csg_vol
+
+#DC / Csg volume
+def dc_csg(seabed, csg_shoe, csg_cap, dp_length, hwdp_length, dc_length, dc_ce_cap, bit_depth):
+
+    if dp_length + hwdp_length < seabed and bit_depth > seabed:
+        dc_csg_vol = (csg_cap - dc_ce_cap) * (bit_depth - seabed)
+
+    elif (dp_length + hwdp_length > seabed and dp_length + hwdp_length < csg_shoe and
+bit_depth <= csg_shoe):
+        dc_csg_vol = (csg_cap - dc_ce_cap) * dc_length
+
+    elif dp_length + hwdp_length < csg_shoe and bit_depth > csg_shoe:
+        dc_csg_vol = (csg_cap - dc_ce_cap) * (csg_shoe - (dp_length + hwdp_length))
+
+    else:
+        dc_csg_vol = 0
+
+    print 'Csg/DC vol = ' + str(dc_csg_vol)
+    return dc_csg_vol
