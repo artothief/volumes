e682864c55f2bfc83aa48f13ac502743dfabb425
artothief <wolden@gmail.com>
2014-06-06 23:38:32 +0200

    fixed database, need to fill in for all entries, combos and also need clear (button maybe?)

--- volumes.py
+++ volumes.py
@@ -2,6 +2,15 @@ __author__ = 'artothief'
 
 from gi.repository import Gtk
 from decimal import *
+import sqlite3
+
+sqlite3.register_adapter(Decimal, lambda x: str(x))
+sqlite3.register_converter('decimal', Decimal)
+
+conn = sqlite3.connect("input.db",
+    detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+c = conn.cursor()
+
 
 from Riser import *
 from Casing import *
@@ -13,7 +22,10 @@ import DC
 
 
 def num(entry):
-    number = Decimal(0.00) if not entry else Decimal(entry)
+    de_com = entry.replace(',', '.')
+    number = Decimal(0.00) if not entry else Decimal(de_com)
+    c.execute('INSERT INTO entries(val) VALUES (?)', (number,))
+    conn.commit()
     return number
 
 
@@ -29,6 +41,20 @@ class Volumes:
         self.dp = Pipe.Add_DP()
         self.dc = DC.Add_DC()
 
+        try:
+            c.execute('SELECT val FROM entries')
+            x = [record[0] for record in c.fetchall()]
+            print x
+        except Exception as e:
+            print 'error'
+            raise e
+
+        def st(entry, dig):
+            if x and x[dig] != '0':
+                entry.set_text(x[dig])
+            else:
+                pass
+
         #making important labels bold
         bold1 = builder.get_object('bold1')
         bold1.set_markup('<b>String Volume :</b>')
@@ -41,18 +67,23 @@ class Volumes:
 
         #bit depth, casing, riser and open hole info
         self.seabed_entry = builder.get_object('seabed_entry')
+        st(self.seabed_entry, 0)
         self.bit_depth_label = builder.get_object('bit_depth_label')
         self.bit_depth_entry = builder.get_object('bit_depth_entry')
+        st(self.bit_depth_entry, 6)
         self.liner_chbutton = builder.get_object('liner_chbutton')
         self.csg_shoe_label = builder.get_object('csg_shoe_label')
         self.csg_shoe_entry = builder.get_object('csg_shoe_entry')
         self.csg_cap_entry = builder.get_object('csg_cap_entry')
         self.liner_shoe_label = builder.get_object('liner_shoe_label')
         self.liner_shoe_entry = builder.get_object('liner_shoe_entry')
+        st(self.liner_shoe_entry, 2)
         self.liner_cap_entry = builder.get_object('liner_cap_entry')
+        st(self.liner_cap_entry, 1)
         self.liner_cap_label = builder.get_object('liner_cap_label')
         self.pbr_label = builder.get_object('pbr_label')
         self.pbr_entry = builder.get_object('pbr_entry')
+        st(self.pbr_entry, 3)
         self.oh_box = builder.get_object('oh_box')
         self.oh_store = builder.get_object('liststore2')
         self.oh_vol_label = builder.get_object('oh_vol_label')
@@ -139,6 +170,8 @@ class Volumes:
             self.liner_cap_label.hide()
 
     def on_calc_button_clicked(self, *args):
+        c.execute('DROP TABLE IF EXISTS entries')
+        c.execute('CREATE TABLE  entries(val TEXT)')
         #get active comboboxes and liststores, entry's first to meet dependencies
         seabed = num(self.seabed_entry.get_text())
         riser_cap = Decimal('187.77')
@@ -146,27 +179,29 @@ class Volumes:
         liner_shoe = num(self.liner_shoe_entry.get_text())
         pbr = num(self.pbr_entry.get_text())
         csg_cap = num(self.csg_cap_entry.get_text())
-        csg_shoe = num(self.csg_shoe_entry.get_text()) if not self.liner_chbutton.get_active() else pbr
+        csg_shoe = num(self.csg_shoe_entry.get_text())
+        if self.liner_chbutton.get_active():
+            csg_shoe = pbr
         bit_depth = num(self.bit_depth_entry.get_text())
         dc_length = num(self.dc_entry.get_text())
         dc_act = self.dc_box.get_active()
-        dc_ce_cap = Decimal(self.dc_store[dc_act] [2])
-        dc_cap = Decimal(self.dc_store[dc_act] [1])
+        dc_ce_cap = Decimal(self.dc_store[dc_act][2])
+        dc_cap = Decimal(self.dc_store[dc_act][1])
         dc_vol = dc_length * dc_cap
         hwdp_length = num(self.hwdp_entry.get_text())
         hwdp_act = self.hwdp_box.get_active()
-        hwdp_cap = Decimal(self.hwdp_store[hwdp_act] [1])
-        hwdp_ce_cap = Decimal(self.hwdp_store[hwdp_act] [2])
+        hwdp_cap = Decimal(self.hwdp_store[hwdp_act][1])
+        hwdp_ce_cap = Decimal(self.hwdp_store[hwdp_act][2])
         hwdp_vol = hwdp_length * hwdp_cap
         dp_length = Decimal(bit_depth - (hwdp_length + dc_length))
-        p_act = self.dp_box.get_active()
-        p_cap = Decimal(self.dp_store[p_act] [1])
-        dp_ce_cap = Decimal(self.dp_store[p_act] [2])
-        dp_vol = dp_length * p_cap
+        dp_act = self.dp_box.get_active()
+        dp_cap = Decimal(self.dp_store[dp_act][1])
+        dp_ce_cap = Decimal(self.dp_store[dp_act][2])
+        dp_vol = dp_length * dp_cap
         oh_act = self.oh_box.get_active()
-        oh_cap = Decimal(self.oh_store[oh_act] [1])
+        oh_cap = Decimal(self.oh_store[oh_act][1])
         mp_liner_act = self.mp_liner_box.get_active()
-        mp_liner_cap = Decimal(self.mp_linerstore[mp_liner_act] [1])
+        mp_liner_cap = Decimal(self.mp_linerstore[mp_liner_act][1])
 
         # Drillstring length and volumes calculations
         self.dp_length_label.set_text(str(dp_length))
@@ -179,7 +214,7 @@ class Volumes:
         riser_volume = dp_riser(seabed, riser_cap, dp_length, dp_ce_cap) +\
                             hwdp_riser(seabed, riser_cap, dp_length, hwdp_length, hwdp_ce_cap) +\
                             dc_riser(seabed, riser_cap, dp_length, hwdp_length, dc_ce_cap, bit_depth)
-        self.riser_vol_label.set_text(str(round(riser_volume, 1)) + ' Litres' )
+        self.riser_vol_label.set_text(str(round(riser_volume, 1)) + ' Litres')
         riser_strokes = riser_volume / mp_liner_cap
         self.riser_stroke_label.set_text(str(int(riser_strokes)) + ' Strokes')
 
