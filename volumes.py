6fad2a52ed9bb7132b1ea8d32d70f33d077afbae
artothief <wolden@gmail.com>
2015-03-17 20:19:11 -0230

    Fixed glitch in string volume

--- volumes.py
+++ volumes.py
@@ -18,7 +18,6 @@ from OH import *
 import Tubulars
 
 
-
 def num(entry, choice):
     if choice == 1:
         de_com = entry.replace(',', '.')
@@ -242,6 +241,7 @@ class Volumes:
         c.execute('DROP TABLE IF EXISTS combos')
         c.execute('CREATE TABLE IF NOT EXISTS  entries(ent TEXT)')
         c.execute('CREATE TABLE IF NOT EXISTS combos(com INTEGER)')
+
         #get active comboboxes and liststores, entry's first to meet dependencies
         seabed = num(self.seabed_entry.get_text(), 1)
         riser_cap = Decimal('187.77')
@@ -255,19 +255,21 @@ class Volumes:
         bit_depth = num(self.bit_depth_entry.get_text(), 1)
         dc_length = num(self.dc_entry.get_text(), 1)
         dc_act = num(self.dc_box.get_active(), 3)
-        dc_ce_cap = num(self.dc_store[dc_act][2], 2) if dc_act else num('0.00', 2)
-        dc_cap = num(self.dc_store[dc_act][1], 2) if dc_act else num('0.00', 2)
+        dc_ce_cap = num(self.dc_store[dc_act][2], 2) if self.dc_store[dc_act][2] else num('0.00', 2)
+        dc_cap = num(self.dc_store[dc_act][1], 2) if self.dc_store[dc_act][1] else num('0.00', 2)
         dc_vol = dc_length * dc_cap
         hwdp_length = num(self.hwdp_entry.get_text(), 1)
         hwdp_act = num(self.hwdp_box.get_active(), 3)
-        hwdp_cap = num(self.hwdp_store[hwdp_act][1], 2) if hwdp_act else num('0.00', 2)
-        hwdp_ce_cap = num(self.hwdp_store[hwdp_act][2], 2) if hwdp_act else num('0.00', 2)
+        hwdp_cap = num(self.hwdp_store[hwdp_act][1], 2) if self.hwdp_store[hwdp_act][1] else num('0.00', 2)
+        hwdp_ce_cap = num(self.hwdp_store[hwdp_act][2], 2) if self.hwdp_store[hwdp_act][2] else num('0.00', 2)
         hwdp_vol = hwdp_length * hwdp_cap
+
+        # Check tapered checkbutton, set dp2 values accordingly
         if self.tap_chbutton.get_active() and self.dp2_box.get_active() >= 0:
             dp2_length = num(self.dp2_entry.get_text(), 1)
             dp2_act = num(self.dp2_box.get_active(), 3)
-            dp2_cap = num(self.dp2_store[dp2_act][1], 2)
-            dp2_ce_cap = num(self.dp2_store[dp2_act][2], 2)
+            dp2_cap = num(self.dp2_store[dp2_act][1], 2) if self.dp2_store[dp2_act][1] else num('0.00', 2)
+            dp2_ce_cap = num(self.dp2_store[dp2_act][2], 2) if self.dp2_store[dp2_act][2] else num('0.00', 2)
         else:
             dp2_length = num('0.00', 1)
             dp2_act = num('0.00', 3)
